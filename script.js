dad9f152e53e5dedfa230d14ef094de77f0e4db6
Felipe Zapata <felipezapata990@gmail.com>
2024-05-05 18:17:19 +0200

    Proyectos fase 1
    Aun faltaria la parte responsive

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const sobremi = document.getElementById("me")
 const titulo = document.getElementById("titulo")
 const habilidades = document.getElementById("habilidades")
 const contactame = document.getElementById("contactame")
+const modalSinretorno = document.getElementById("modal-sinretorno")
+const modalCerrar = document.getElementById("modal-cerrar")
+const modalAbrir = document.getElementById("sinretorno")
+const iframe1 = document.getElementById("iframe1")
 
 function proyectos() {
     titulo.textContent = "Proyectos";
@@ -20,3 +24,15 @@ function sobre_mi() {
     contactame.style.display = "none";
 }
 
+modalAbrir.addEventListener("click", ()=> { 
+  modalSinretorno.showModal(); 
+  iframe1.src = 'https://sinretorno.es'
+  iframe1.style.display = 'block';
+})
+modalCerrar.addEventListener("click", ()=> { modalSinretorno.close(iframe1.src = ' '); })
+modalSinretorno.addEventListener('click', (e) => {
+  if (e.target === modalSinretorno) {
+    iframe1.src = ' '
+    modalSinretorno.close();
+  }
+});
