c4a4705332d86cb7e19b34746a56dcc3514bc878
Felipe Zapata <felipezapata990@gmail.com>
2024-05-14 19:09:53 +0200

    Mejora de UI
    Primer cambio  de UI para la mejora del responsive

--- script.js
+++ script.js
@@ -5,8 +5,21 @@ const habilidad = document.getElementById("habilidades")
 const contactame = document.getElementById("contactame")
 const modalSinretorno = document.getElementById("modal-sinretorno")
 const modalCerrar = document.getElementById("modal-cerrar")
-const modalAbrir = document.getElementById("sinretorno")
+const modalCerrar2 = document.getElementById("modal-cerrar2")
+const modalAbrirSinRetorno = document.getElementById("sinretorno")
+const modalAbrirK96Store = document.getElementById("k96")
+const modalK96Store = document.getElementById("modal-k96")
 const iframe1 = document.getElementById("iframe1")
+const iframe2 = document.getElementById("iframe2")
+
+
+function contactos() {
+  titulo.textContent = "Contáctame";
+  proyecto.style.display = "none";
+  habilidad.style.display = "none";
+  sobremi.style.display = "none";
+  contactame.style.display = "flex";
+}
 
 function habilidades() {
   titulo.textContent = "Habilidades";
@@ -32,7 +45,7 @@ function sobre_mi() {
     contactame.style.display = "none";
 }
 
-modalAbrir.addEventListener("click", ()=> { 
+modalAbrirSinRetorno.addEventListener("click", ()=> { 
   modalSinretorno.showModal(); 
   iframe1.src = 'https://sinretorno.es'
   iframe1.style.display = 'block';
@@ -42,5 +55,18 @@ modalSinretorno.addEventListener('click', (e) => {
   if (e.target === modalSinretorno) {
     iframe1.src = ' '
     modalSinretorno.close();
+  }
+});
+
+modalAbrirK96Store.addEventListener("click", ()=> { 
+  modalK96Store.showModal(); 
+  iframe2.src = 'https://k96store.com/'
+  iframe2.style.display = 'block';
+})
+modalCerrar2.addEventListener("click", ()=> { modalK96Store.close(iframe2.src = ' '); })
+modalK96Store.addEventListener('click', (e) => {
+  if (e.target === modalK96Store) {
+    iframe2.src = ' '
+    modalK96Store.close();
   }
 });
